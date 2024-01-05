30662c384ca66574bd9b74fc51bb9a788456b6b2
GitHub <noreply@github.com>
2024-01-05 06:06:41 +0000

    chore(deps): Update deno dependencies

--- denops/autobackup/main.ts
+++ denops/autobackup/main.ts
@@ -6,15 +6,15 @@
 
 import * as autocmd from "https://deno.land/x/denops_std@v5.2.0/autocmd/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v5.2.0/function/mod.ts";
-import * as fs from "https://deno.land/std@0.210.0/fs/mod.ts";
+import * as fs from "https://deno.land/std@0.211.0/fs/mod.ts";
 import * as helper from "https://deno.land/x/denops_std@v5.2.0/helper/mod.ts";
 import * as op from "https://deno.land/x/denops_std@v5.2.0/option/mod.ts";
-import * as path from "https://deno.land/std@0.210.0/path/mod.ts";
+import * as path from "https://deno.land/std@0.211.0/path/mod.ts";
 import * as lambda from "https://deno.land/x/denops_std@v5.2.0/lambda/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
 import { batch } from "https://deno.land/x/denops_std@v5.2.0/batch/mod.ts";
-import { walk } from "https://deno.land/std@0.210.0/fs/walk.ts";
-import { format } from "https://deno.land/std@0.210.0/datetime/mod.ts";
+import { walk } from "https://deno.land/std@0.211.0/fs/walk.ts";
+import { format } from "https://deno.land/std@0.211.0/datetime/mod.ts";
 import dir from "https://deno.land/x/dir@1.5.2/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
 import { Semaphore } from "https://deno.land/x/async@v2.0.2/mod.ts";
