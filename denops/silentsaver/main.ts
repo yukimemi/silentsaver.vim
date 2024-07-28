2166baf443ec03c86675db163243aff8bc9ef1f1
yukimemi <yukimemi@gmail.com>
2024-07-28 21:29:52 +0900

    :zap: Use jsr !!!

--- denops/silentsaver/main.ts
+++ denops/silentsaver/main.ts
@@ -1,24 +1,24 @@
 // =============================================================================
 // File        : main.ts
 // Author      : yukimemi
-// Last Change : 2024/03/02 22:58:00.
+// Last Change : 2024/07/28 21:29:28.
 // =============================================================================
 
-import * as autocmd from "https://deno.land/x/denops_std@v6.5.1/autocmd/mod.ts";
-import * as fn from "https://deno.land/x/denops_std@v6.5.1/function/mod.ts";
-import * as fs from "https://deno.land/std@0.224.0/fs/mod.ts";
-import * as helper from "https://deno.land/x/denops_std@v6.5.1/helper/mod.ts";
-import * as op from "https://deno.land/x/denops_std@v6.5.1/option/mod.ts";
-import * as path from "https://deno.land/std@0.224.0/path/mod.ts";
-import * as lambda from "https://deno.land/x/denops_std@v6.5.1/lambda/mod.ts";
-import * as vars from "https://deno.land/x/denops_std@v6.5.1/variable/mod.ts";
-import { batch } from "https://deno.land/x/denops_std@v6.5.1/batch/mod.ts";
-import { walk } from "https://deno.land/std@0.224.0/fs/walk.ts";
-import { format } from "https://deno.land/std@0.224.0/datetime/mod.ts";
-import dir from "https://deno.land/x/dir@1.5.2/mod.ts";
-import type { Denops } from "https://deno.land/x/denops_std@v6.5.1/mod.ts";
-import { Semaphore } from "https://deno.land/x/async@v2.1.0/mod.ts";
-import { assert, ensure, is } from "https://deno.land/x/unknownutil@v3.18.1/mod.ts";
+import * as autocmd from "jsr:@denops/std@7.0.0/autocmd";
+import * as fn from "jsr:@denops/std@7.0.0/function";
+import * as fs from "jsr:@std/fs@0.224.0";
+import * as helper from "jsr:@denops/std@7.0.0/helper";
+import * as lambda from "jsr:@denops/std@7.0.0/lambda";
+import * as op from "jsr:@denops/std@7.0.0/option";
+import * as path from "jsr:@std/path@0.224.0";
+import * as vars from "jsr:@denops/std@7.0.0/variable";
+import type { Denops } from "jsr:@denops/std@7.0.0";
+import { Semaphore } from "jsr:@lambdalisue/async@2.1.1";
+import { assert, ensure, is } from "jsr:@core/unknownutil@3.18.1";
+import { batch } from "jsr:@denops/std@7.0.0/batch";
+import { dir } from "jsr:@cross/dir@1.1.0";
+import { format } from "jsr:@std/datetime@0.224.0";
+import { walk } from "jsr:@std/fs@0.224.0";
 
 let debug = false;
 let enable = true;
@@ -27,7 +27,7 @@ let backupNotify = false;
 let ignoreFileTypes = ["log"];
 let uiSelect = false;
 const files = new Map<string, string>();
-const home = ensure(dir("home"), is.String);
+const home = ensure(await dir("home"), is.String);
 let backup_dir = path.join(home, ".cache", "silentsaver");
 
 let events: autocmd.AutocmdEvent[] = [
